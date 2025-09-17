fed881be552b06081f4fd16a2e942fc19295168b
jann-s <56644544+jann-s@users.noreply.github.com>
2025-09-17 09:34:05 +0200

    Create tracking folder

---
+++ tracking/__init__.py
@@ -0,0 +1,5 @@
+from .clustering import TeamClusterer
+from .roster import TeamRoster
+from .viz import draw_team_rings
+from .utils import tracker_cfg_path
+from .features import jersey_patch, hsv_hist_feature
